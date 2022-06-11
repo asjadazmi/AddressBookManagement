83622c6db5df827baaa4940559a3d8e527bb97dd
S MD ASJAD AZMI <asjadazmi0786@gmail.com>
2022-06-11 18:11:16 +0530

    Added : Adding the contact in Address book

--- src/com/management/AddressBookMain.java
+++ src/com/management/AddressBookMain.java
@@ -1,8 +1,34 @@
 package com.management;
 
-public class AddressBookMain {
+import java.util.Scanner;
+
+public class AddressBookMain extends Contact {
+
 	public static void main(String[] args) {
 		System.out.println("welcome to the address book management");
+		   Scanner sc=new Scanner(System.in);
+			System.out.println("enter the first name");
+			first_name=sc.next();
+			System.out.println("enter the last name");
+			last_name=sc.next();
+			System.out.println("enter the Address");
+			Address=sc.next();
+			System.out.println("enter the city");
+			city=sc.next();
+			System.out.println("enter the state");
+			state=sc.next();
+			System.out.println("enter the zip");
+			zip=sc.nextInt();
+			System.out.println("enter the phone number");
+			phone_number=sc.nextInt();
+			System.out.println("enter the email");
+			email=sc.next();
+			
+			
+			AddressBookMain c1=new AddressBookMain(); 
+			System.out.println("my name is :"+c1.first_name+" "+c1.last_name+"\nAddress: "+c1.Address+".\nmy city :"+c1.city+"\nmy state"+c1.state+"\nphone number:"+c1.phone_number+"\nemail"+c1.email);
+			
+		
 	}
 	
 }
--- src/com/management/Contact.java
+++ src/com/management/Contact.java
@@ -10,6 +10,75 @@ public class Contact {
 	public static long phone_number;
 	public static String email;
 	
+	public Contact(String first_name, String last_name,String Address,String city,String state,long phone_number,String email) {
+		this.first_name=first_name;
+		this.last_name=last_name;
+		this.Address=Address;
+		this.city=city;
+		this.state=state;
+		this.zip=zip;
+		this.phone_number=phone_number;
+		this.email=email;
+		
+		
+	}
 
+
+	public static String getFirst_name() {
+		return first_name;
+	}
+	public static void setFirst_name(String first_name) {
+		Contact.first_name = first_name;
+	}
+	public static String getLast_name() {
+		return last_name;
+	}
+	public static void setLast_name(String last_name) {
+		Contact.last_name = last_name;
+	}
+	public static String getAddress() {
+		return Address;
+	}
+	public static void setAddress(String address) {
+		Address = address;
+	}
+	public static String getCity() {
+		return city;
+	}
+	public static void setCity(String city) {
+		Contact.city = city;
+	}
+	public static String getState() {
+		return state;
+	}
+	public static void setState(String state) {
+		Contact.state = state;
+	}
+	public static int getZip() {
+		return zip;
+	}
+	public static void setZip(int zip) {
+		Contact.zip = zip;
+	}
+	public static long getPhone_number() {
+		return phone_number;
+	}
+	public static void setPhone_number(long phone_number) {
+		Contact.phone_number = phone_number;
+	}
+	public static String getEmail() {
+		return email;
+	}
+	public static void setEmail(String email) {
+		Contact.email = email;
+	}
+
+	@Override
+	public String toString() {
+		return "Contact []";
+	}
+
+	
+	
 
 }
