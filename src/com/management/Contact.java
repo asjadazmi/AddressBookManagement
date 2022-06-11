b0f377e52b91322cadb7df33e148510a1a760adf
S MD ASJAD AZMI <asjadazmi0786@gmail.com>
2022-06-11 17:28:05 +0530

    Added : Created the required contact for Address book Mnagement System

---
+++ src/com/management/Contact.java
@@ -0,0 +1,15 @@
+package com.management;
+
+public class Contact {
+	public static String first_name;
+	public static String last_name;
+	public static String Address;
+	public static String city;
+	public static String state;
+	public static int zip;
+	public static long phone_number;
+	public static String email;
+	
+
+
+}
